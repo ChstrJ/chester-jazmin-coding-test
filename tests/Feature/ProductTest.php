f9dab2998c413ceb40833d361628566dd6c8817a
Chester <89991461+ChesterJaz@users.noreply.github.com>
2024-06-20 13:40:22 +0800

    unit test: test create product

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -20,6 +20,40 @@ class ProductTest extends TestCase
         $response->assertStatus(200)->assertJsonCount(15, 'data');
     }
 
-    
+    public function testCreateProduct()
+    {
+
+        $data = [
+            'name' => 'test',
+            'description' => 'test',
+            'price' => 6.90
+        ];
+
+        $response = $this->postJson('/api/products', $data);
+        $response->assertStatus(201)
+            ->assertJsonStructure([
+                'status',
+                'message',
+                'data' => [
+                    'id',
+                    'Product name',
+                    'Product description',
+                    'Product price',
+                    'Created at',
+                    'Updated at'
+                ]
+            ])
+            ->assertJson([
+                'status' => 'success',
+                'message' => 'Product created successfully.',
+                'data' => [
+                    'Product name' => 'test',
+                    'Product description' => 'test',
+                    'Product price' => 6.9
+                ]
+            ]);
+    }
+
+   
 
 }
